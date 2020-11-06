56fea297947a709b7c0ca2310a12d91b2c7750b1
roblabla <unfiltered@roblab.la>
2020-11-06 11:09:56 +0000

    Add blink on os cycle

--- keyboards/ergodox_ez/keymaps/roblabla/keymap.c
+++ keyboards/ergodox_ez/keymaps/roblabla/keymap.c
@@ -82,6 +82,8 @@ enum os {
 };
 
 static enum os OS_TY = Windows;
+static char BLINK = 0;
+static uint32_t CHANGE_TICK = 0;
 
 static void tap_keypad_digit(uint8_t digit) {
   if (digit == 0)
@@ -140,7 +142,7 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_ergodox_pretty(
     KC_EQUAL,       KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_LEFT,                                        KC_RIGHT,       KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,
-    TO(4),          KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           TG(1),                                          TG(1),          KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLASH,
+    TO(4),          KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLASH,
     MO(3),          KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                                                           KC_H,           KC_J,           KC_K,           KC_L,           LT(2,KC_SCOLON),LGUI_T(KC_QUOTE),
     KC_LSHIFT,      LCTL_T(KC_Z),   KC_X,           KC_C,           KC_V,           KC_B,           KC_TRANSPARENT,                                 KC_MEH,         KC_N,           KC_M,           KC_COMMA,       KC_DOT,         RCTL_T(KC_SLASH),KC_RSHIFT,
     LT(1,KC_GRAVE), KC_QUOTE,       KC_LALT,        KC_LEFT,        KC_RIGHT,                                                                                                       KC_UP,          KC_DOWN,        KC_LBRACKET,    KC_RBRACKET,    MO(1),
@@ -192,7 +194,36 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 };
 
+void rgb_matrix_indicators_user(void) {
+  if (g_suspend_state || keyboard_config.disable_layer_led) { return; }
 
+  if (!BLINK)
+    return;
+
+  uint16_t delta = timer_elapsed32(CHANGE_TICK);
+
+  if (BLINK && delta > 5000)
+    BLINK = 0;
+
+  int led_to_blink;
+  switch (OS_TY) {
+    case Windows:
+      led_to_blink = 32;
+      break;
+    case MacOS:
+      led_to_blink = 16;
+      break;
+    default:
+      led_to_blink = 13;
+      break;
+  }
+
+  if (delta % 2500 < 1250) {
+    rgb_matrix_set_color( led_to_blink, 255, 255, 255 );
+  } else {
+    rgb_matrix_set_color( led_to_blink, 0, 0, 0 );
+  }
+}
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
@@ -214,6 +245,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case OS_CYCLE:
       if (record->event.pressed) {
         OS_TY = (OS_TY + 1) % OS_END;
+        CHANGE_TICK = timer_read32();
+        BLINK = 1;
       }
       return false;
     case RGB_SLD:
